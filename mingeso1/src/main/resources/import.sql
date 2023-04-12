05a470ca6708d65cbb54d331ab92aacde9ed30f2
Ignacio Villarroel <ignacio.villarroel.e@usach.cl>
2023-04-12 14:00:19 -0400

    Implementación de Navbar para archivos .css

---
+++ mingeso1/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("13001", "Si", "A", "Carlos Retamales");
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("05002", "No", "B", "Ignacio Villaroel");
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("10003", "No", "C", "Ian Rickmers");
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("13004", "Si", "D", "Leo Vergara");
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("04005", "Si", "D", "Joakin Roa");
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("05006", "No", "A", "Isabella Perez");
