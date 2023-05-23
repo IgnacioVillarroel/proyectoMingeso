3f0ae527f284eeef8171c318768d22a981890107
Ignacio Villarroel <ignacio.villarroel.e@usach.cl>
2023-05-22 20:03:13 -0400

    Cambios para presentación

--- mingeso1/src/main/resources/import.sql
+++ mingeso1/src/main/resources/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("13001", "Si", "A", "Carlos Retamales");
-INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("05002", "No", "B", "Ignacio Villaroel");
-INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("10003", "No", "C", "Ian Rickmers");
-INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("13004", "Si", "D", "Leo Vergara");
-INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("04005", "Si", "D", "Joakin Roa");
-INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("05006", "No", "A", "Isabella Perez");
-INSERT INTO pagos(id_pago,quincena,codigo_proveedor,nombre_proveedor,total_kls,dias,promedio_kls,variacion_leche,grasa,variacion_grasa,solidos_totales,variacion_st,pago_leche,pago_grasa,pago_st,bono,dcto_leche,dcto_grasa,dcto_st,total,monto_retencion,monto_final) VALUES (0,"2023/02/Q2","13001","Carlos Reamales",1500.0,9.0,80.0,0.0,60.0,0.0,30.0,0.0,700000.0,80000.0,-90000.0,200.0,0.0,0.0,0.0,0.0,0.0,690000.0);
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("11111", "Si", "A", "Empresa Leche Colo Colo");
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("22222", "No", "B", "Empresa Leche Rangers");
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("33333", "No", "C", "Empresa Leche Union Española");
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("44444", "Si", "D", "Empresa Leche Audax Italiano");
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("55555", "Si", "D", "Empresa Leche Santiago Morning");
+INSERT INTO proveedores(codigo,afecto_retencion,categoria,nombre_proveedor) VALUES ("05006", "No", "A", "Empresa Leche Everton");
+INSERT INTO pagos(id_pago,quincena,codigo_proveedor,nombre_proveedor,total_kls,dias,promedio_kls,variacion_leche,grasa,variacion_grasa,solidos_totales,variacion_st,pago_leche,pago_grasa,pago_st,bono,dcto_leche,dcto_grasa,dcto_st,total,monto_retencion,monto_final) VALUES (0,"2023/02/Q2","11111","Empresa Leche Colo Colo",1500.0,9.0,80.0,0.0,60.0,0.0,30.0,0.0,700000.0,80000.0,-90000.0,200.0,0.0,0.0,0.0,0.0,0.0,690000.0);
